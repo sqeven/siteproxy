b47c2db782a54a1ef9de27b9731f60472cfbad34
Sqeven <sqeven@qq.com>
2020-06-28 11:30:15 +0800

    perf: 更新代理

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'proxy.netptop.com',
+    serverName: 'siteproxy.sqeven.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
